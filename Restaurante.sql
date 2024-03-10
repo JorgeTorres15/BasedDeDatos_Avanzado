be02ab44582e89505b20c57ce0a7a1ee116373c7
SQLote <162851303+SQLote@users.noreply.github.com>
2024-03-09 20:40:13 -0800

    Update Restaurante.sql
    aceptala ya estan todas las tablas solo hare mas tarde los storeprocedures y eso

--- Restaurante.sql
+++ Restaurante.sql
@@ -1,2 +1,94 @@
-CREATE DATABASE RestauranteDB;
-USE RestauranteDB;
+CREATE DATABASE ChinaTown;
+USE ChinaTown;
+
+create table if not exists Tipo_cliente (
+	ID_Tipocliente int primary key auto_increment,
+    Nombre varchar(50) not null
+    );
+    
+create table if not exists Clientes(
+	ID_Cliente int primary key auto_increment,
+    ID_Tipocliente int,
+	foreign key (ID_Tipocliente) references Tipo_cliente(ID_Tipocliente),
+    Nombre varchar(50),
+    Telefono int 
+    );
+    
+create table if not exists Rol(
+	ID_Rol int primary key auto_increment,
+    Nombre varchar (50)
+    );
+
+create table if not exists Personal(
+	ID_Personal int primary key auto_increment,
+    Nombre varchar(50) not null,
+    Apellido varchar(50) not null,
+    Telefono int not null,
+    Sueldo float not null,
+    Fecha_ingreso datetime not null,
+    ID_Rol int not null,
+    foreign key (ID_Rol) references Rol(ID_Rol)
+    );
+
+create table if not exists Tipo_Provedores(
+	ID_Tipoprovedores int primary key auto_increment,
+    Nombre varchar(50)
+    );
+    
+create table if not exists Provedores(
+	ID_Provedor int primary key auto_increment,
+    Nombre varchar(50) not null,
+    ID_Tipoprovedores int not null,
+    foreign key (ID_Tipoprovedores) references Tipo_Provedores(ID_Tipoprovedores),
+    Telefono int not null,
+    Correo varchar(50) default "Sin correo"
+    );
+
+create table if not exists Materiales (
+	ID_Material int primary key auto_increment,
+    Nombre varchar(50) not null,
+    Stock int not null,
+    ID_Provedor int,
+    foreign key (ID_Provedor) references Provedores(ID_Provedor)
+    );
+
+create table if not exists Ingredientes(
+	ID_Ingredientes int primary key auto_increment,
+    ID_Provedor int not null,
+    Nombre varchar(50) not null,
+    Fecha_Recepcion datetime not null,
+    Concervacion varchar(50) not null,
+    foreign key (ID_Provedor) references Provedores(ID_Provedor)
+    );
+
+create table if not exists Categorias(
+	ID_Categoria int primary key auto_increment,
+    Nombre varchar(50)
+    );
+    
+create table if not exists Platillos(
+	ID_Platillo int primary key auto_increment,
+	Nombre varchar(50) not null,
+    Descripcion varchar(50) not null,
+    Precio float not null,
+    ID_Categoria int not null,
+    ID_Ingredientes int not null,
+    foreign key (ID_Categoria) references Categorias (ID_Categoria),
+    foreign key (ID_Ingredientes) references Ingredientes(ID_Ingredientes)
+    );
+    
+    create table if not exists Pedidos(
+		ID_Pedido int primary key auto_increment,
+        ID_Cliente int not null,
+        ID_Personal int not null,
+        ID_Platillo int not null,
+        Total float not null ,
+        Fecha datetime not null,
+        Entregado varchar(2) not null default "No",
+		foreign key (ID_Cliente) references Clientes(ID_Cliente),
+		foreign key (ID_Personal) references Personal(ID_Personal),
+		foreign key (ID_Platillo) references Platillos(ID_Platillo)
+        );
+
+
+show tables;
