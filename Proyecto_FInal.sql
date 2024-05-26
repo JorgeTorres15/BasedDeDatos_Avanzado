05f176edb9662f4bd29840eefd6b92caff692c59
Mr.ExtremeProgramming <162726095+JorgeTorres15@users.noreply.github.com>
2024-05-26 14:10:48 -0700

    Update Proyecto_FInal.sql

--- Proyecto_FInal.sql
+++ Proyecto_FInal.sql
@@ -70,7 +70,13 @@ CREATE TABLE Ventas (
     FOREIGN KEY (Tipo_Venta_ID) REFERENCES Tipo_Venta(id) ON DELETE SET NULL
 );
 
-select * from Ventas;
+-- Creacion de la tabla Criticas
+CREATE TABLE Criticas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    videojuego_id INT,
+    critica_calidad_precio ENUM('Buena', 'Mala'),
+    FOREIGN KEY (videojuego_id) REFERENCES Videojuegos(id)
+);
 
 CREATE TABLE Registro_Operaciones (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -79,9 +85,6 @@ CREATE TABLE Registro_Operaciones (
     Detalles TEXT,
     Fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-
-
 
 -- Inserts para la tabla Plataforma
 INSERT INTO Plataforma (Nombre) VALUES 
@@ -206,175 +209,13 @@ INSERT INTO Ventas (Videojuego_ID, Tipo_Venta_ID, Fecha, Cantidad, Precio) VALUE
 (10, 1, '2021-10-10', 4000, 59.99), -- Marvels Spider-Man, Fisica
 (10, 2, '2021-10-15', 3000, 59.99); -- Marvels Spider-Man, En Linea
 
-
--- Creación de la tabla Criticas
-CREATE TABLE Criticas (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    videojuego_id INT,
-    critica_calidad_precio ENUM('Buena', 'Mala'),
-    FOREIGN KEY (videojuego_id) REFERENCES Videojuegos(id)
-);
-
--- Inserción de datos de ejemplo en la tabla Criticas
+-- Insercion de datos de ejemplo en la tabla Criticas
 INSERT INTO Criticas (videojuego_id, critica_calidad_precio) VALUES
 (1, 'Buena'),
 (2, 'Mala'),
 (3, 'Buena'),
 (4, 'Mala'),
 (5, 'Buena');
-
-
--- Procedimientos para la tabla Plataforma
-DELIMITER //
-CREATE PROCEDURE CrearPlataforma(IN platform_name VARCHAR(255))
-BEGIN
-    INSERT INTO Plataforma (Nombre) VALUES (platform_name);
-END //
-DELIMITER ;
-
-DELIMITER //
-CREATE PROCEDURE GetAllPlataformas()
-BEGIN
-    SELECT * FROM Plataforma;
-END //
-DELIMITER ;
-
-DELIMITER //
-CREATE PROCEDURE GetByIdPlataforma(IN platform_id INT)
-BEGIN
-    SELECT * FROM Plataforma WHERE id = platform_id;
-END //
-DELIMITER ;
-
-DELIMITER //
-CREATE PROCEDURE UpdateByIdPlataforma(IN platform_id INT, IN platform_name VARCHAR(255))
-BEGIN
-    UPDATE Plataforma SET Nombre = platform_name WHERE id = platform_id;
-END //
-DELIMITER ;
-
-DELIMITER //
-CREATE PROCEDURE DeleteAllPlataformas()
-BEGIN
-    DELETE FROM Plataforma;
-END //
-DELIMITER ;
-
-DELIMITER //
-CREATE PROCEDURE DeleteById
-
-
-
-
-
-
-
-
-
-
-
-CREATE TABLE Registro_Operaciones (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    Tabla VARCHAR(255),
-    Operacion VARCHAR(50),
-    Detalles TEXT,
-    Fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
--- Inserts para la tabla Plataforma
-INSERT INTO Plataforma (Nombre) VALUES 
-('PlayStation 5'), 
-('Xbox Series X'), 
-('Nintendo Switch'), 
-('PC'), 
-('PlayStation 4'), 
-('Xbox One'), 
-('iOS'), 
-('Android'), 
-('Stadia'), 
-('PlayStation 3');
-
--- Inserts para la tabla Desarrollador
-INSERT INTO Desarrollador (Nombre) VALUES 
-('Naughty Dog'), 
-('CD Projekt Red'), 
-('Rockstar Games'), 
-('Ubisoft'), 
-('Bungie'), 
-('Bethesda'), 
-('Square Enix'), 
-('Capcom'), 
-('Valve'), 
-('Insomniac Games');
-
--- Inserts para la tabla Publicado_por
-INSERT INTO Publicado_por (Nombre) VALUES 
-('Sony Interactive Entertainment'), 
-('Microsoft'), 
-('Nintendo'), 
-('Ubisoft'), 
-('Bethesda Softworks'), 
-('Square Enix'), 
-('Activision'), 
-('Electronic Arts'), 
-('Capcom'), 
-('Warner Bros. Interactive Entertainment');
-
--- Inserts para la tabla Genero
-INSERT INTO Genero (Nombre) VALUES 
-('Accion'), 
-('Aventura'), 
-('RPG'), 
-('Shooter'), 
-('Plataformas'), 
-('Estrategia'), 
-('Deportes'), 
-('Carreras'), 
-('Lucha'), 
-('Simulacion');
-
--- Inserts para la tabla Clasificacion_Edad
-INSERT INTO Clasificacion_Edad (Nombre) VALUES 
-('E (Everyone)'), 
-('E10+ (Everyone 10 and older)'), 
-('T (Teen)'), 
-('M (Mature 17+)'), 
-('AO (Adults Only 18+)'), 
-('RP (Rating Pending)'), 
-('C (Children)'), 
-('GA (General Audience)'), 
-('MA (Mature Audience)'), 
-('A (Adults)');
-
--- Inserts para la tabla Tipo_Plataforma
-INSERT INTO Tipo_Plataforma (Tipo) VALUES 
-('Consola'), 
-('PC'), 
-('Movil'), 
-('Nube'), 
-('VR'), 
-('AR'), 
-('Web'), 
-('Hibrido'), 
-('Portatil'), 
-('Arcade');
-
--- Inserts para la tabla Videojuegos
-INSERT INTO Videojuegos (
-    Nombre, Fecha_Lanzamiento, Meta_Score, Catalogado_por_Meta, 
-    User_Score, Catalogado_por_Usuario, Plataforma_ID, Desarrollador_ID, 
-    Publicado_por_ID, Genero_ID, Clasificacion_Edad_ID, Tipo_Plataforma_ID
-) VALUES 
-('The Last of Us Part II', '2020-06-19', 93, 'Metacritic', 9.3, 'Usuarios', 1, 1, 1, 1, 4, 1),
-('Cyberpunk 2077', '2020-12-10', 86, 'Metacritic', 7.0, 'Usuarios', 2, 2, 2, 3, 4, 2),
-('Red Dead Redemption 2', '2018-10-26', 97, 'Metacritic', 9.7, 'Usuarios', 4, 3, 2, 2, 4, 2),
-('Assassins Creed Valhalla', '2020-11-10', 84, 'Metacritic', 8.2, 'Usuarios', 2, 4, 4, 2, 4, 1),
-('Destiny 2', '2017-09-06', 85, 'Metacritic', 8.5, 'Usuarios', 1, 5, 7, 4, 3, 1),
-('The Elder Scrolls V: Skyrim', '2011-11-11', 94, 'Metacritic', 9.4, 'Usuarios', 4, 6, 5, 3, 4, 2),
-('Final Fantasy VII Remake', '2020-04-10', 87, 'Metacritic', 8.7, 'Usuarios', 1, 7, 6, 3, 3, 1),
-('Resident Evil 3', '2020-04-03', 79, 'Metacritic', 7.9, 'Usuarios', 1, 8, 9, 2, 4, 1),
-('Half-Life: Alyx', '2020-03-23', 92, 'Metacritic', 9.2, 'Usuarios', 4, 9, 10, 1, 4, 5),
-('Marvels Spider-Man', '2018-09-07', 87, 'Metacritic', 8.7, 'Usuarios', 1, 10, 1, 2, 3, 1);
 
 -- Procedimientos para la tabla Plataforma
 DELIMITER //
@@ -727,6 +568,142 @@ BEGIN
 END //
 DELIMITER ;
 
+-- Procedimientos para la tabla Tipo_Venta
+DELIMITER //
+CREATE PROCEDURE CrearTipoVenta(IN sale_type VARCHAR(255))
+BEGIN
+    INSERT INTO Tipo_Venta (Tipo) VALUES (sale_type);
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE GetAllTiposVenta()
+BEGIN
+    SELECT * FROM Tipo_Venta;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE GetByIdTipoVenta(IN sale_type_id INT)
+BEGIN
+    SELECT * FROM Tipo_Venta WHERE id = sale_type_id;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE UpdateByIdTipoVenta(IN sale_type_id INT, IN sale_type VARCHAR(255))
+BEGIN
+    UPDATE Tipo_Venta SET Tipo = sale_type WHERE id = sale_type_id;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE DeleteAllTiposVenta()
+BEGIN
+    DELETE FROM Tipo_Venta;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE DeleteByIdTipoVenta(IN sale_type_id INT)
+BEGIN
+    DELETE FROM Tipo_Venta WHERE id = sale_type_id;
+END //
+DELIMITER ;
+
+-- Procedimientos para la tabla Ventas
+DELIMITER //
+CREATE PROCEDURE CrearVenta(
+    IN videojuego_id INT, IN tipo_venta_id INT, IN fecha DATE, 
+    IN cantidad INT, IN precio DECIMAL(10, 2))
+BEGIN
+    INSERT INTO Ventas (Videojuego_ID, Tipo_Venta_ID, Fecha, Cantidad, Precio) 
+    VALUES (videojuego_id, tipo_venta_id, fecha, cantidad, precio);
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE GetAllVentas()
+BEGIN
+    SELECT * FROM Ventas;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE GetByIdVenta(IN venta_id INT)
+BEGIN
+    SELECT * FROM Ventas WHERE id = venta_id;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE UpdateByIdVenta(
+    IN venta_id INT, IN videojuego_id INT, IN tipo_venta_id INT, 
+    IN fecha DATE, IN cantidad INT, IN precio DECIMAL(10, 2))
+BEGIN
+    UPDATE Ventas 
+    SET Videojuego_ID = videojuego_id, Tipo_Venta_ID = tipo_venta_id, 
+        Fecha = fecha, Cantidad = cantidad, Precio = precio 
+    WHERE id = venta_id;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE DeleteAllVentas()
+BEGIN
+    DELETE FROM Ventas;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE DeleteByIdVenta(IN venta_id INT)
+BEGIN
+    DELETE FROM Ventas WHERE id = venta_id;
+END //
+DELIMITER ;
+
+-- Procedimientos para la tabla Criticas
+DELIMITER //
+CREATE PROCEDURE CrearCritica(IN videojuego_id INT, IN critica_calidad_precio ENUM('Buena', 'Mala'))
+BEGIN
+    INSERT INTO Criticas (videojuego_id, critica_calidad_precio) VALUES (videojuego_id, critica_calidad_precio);
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE GetAllCriticas()
+BEGIN
+    SELECT * FROM Criticas;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE GetByIdCritica(IN critica_id INT)
+BEGIN
+    SELECT * FROM Criticas WHERE id = critica_id;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE UpdateByIdCritica(IN critica_id INT, IN videojuego_id INT, IN critica_calidad_precio ENUM('Buena', 'Mala'))
+BEGIN
+    UPDATE Criticas SET videojuego_id = videojuego_id, critica_calidad_precio = critica_calidad_precio WHERE id = critica_id;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE DeleteAllCriticas()
+BEGIN
+    DELETE FROM Criticas;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE DeleteByIdCritica(IN critica_id INT)
+BEGIN
+    DELETE FROM Criticas WHERE id = critica_id;
+END //
+DELIMITER ;
 
 -- Trigger para la tabla Plataforma
 DELIMITER //
@@ -923,3 +900,172 @@ BEGIN
     INSERT INTO Registro_Operaciones (Tabla, Operacion, Detalles) VALUES ('Videojuegos', 'Delete', CONCAT('Se elimino el videojuego con ID: ', OLD.id));
 END //
 DELIMITER ;
+
+-- Trigger para la tabla Tipo_Venta
+DELIMITER //
+CREATE TRIGGER Tipo_Venta_Insert_Trigger
+AFTER INSERT ON Tipo_Venta
+FOR EACH ROW
+BEGIN
+    INSERT INTO Registro_Operaciones (Tabla, Operacion, Detalles) VALUES ('Tipo_Venta', 'Insert', CONCAT('Se inserto el tipo de venta con ID: ', NEW.id));
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER Tipo_Venta_Update_Trigger
+AFTER UPDATE ON Tipo_Venta
+FOR EACH ROW
+BEGIN
+    INSERT INTO Registro_Operaciones (Tabla, Operacion, Detalles) VALUES ('Tipo_Venta', 'Update', CONCAT('Se actualizo el tipo de venta con ID: ', NEW.id));
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER Tipo_Venta_Delete_Trigger
+AFTER DELETE ON Tipo_Venta
+FOR EACH ROW
+BEGIN
+    INSERT INTO Registro_Operaciones (Tabla, Operacion, Detalles) VALUES ('Tipo_Venta', 'Delete', CONCAT('Se elimino el tipo de venta con ID: ', OLD.id));
+END //
+DELIMITER ;
+
+-- Trigger para la tabla Ventas
+DELIMITER //
+CREATE TRIGGER Ventas_Insert_Trigger
+AFTER INSERT ON Ventas
+FOR EACH ROW
+BEGIN
+    INSERT INTO Registro_Operaciones (Tabla, Operacion, Detalles) VALUES ('Ventas', 'Insert', CONCAT('Se inserto una venta con ID: ', NEW.id));
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER Ventas_Update_Trigger
+AFTER UPDATE ON Ventas
+FOR EACH ROW
+BEGIN
+    INSERT INTO Registro_Operaciones (Tabla, Operacion, Detalles) VALUES ('Ventas', 'Update', CONCAT('Se actualizo una venta con ID: ', NEW.id));
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER Ventas_Delete_Trigger
+AFTER DELETE ON Ventas
+FOR EACH ROW
+BEGIN
+    INSERT INTO Registro_Operaciones (Tabla, Operacion, Detalles) VALUES ('Ventas', 'Delete', CONCAT('Se elimino una venta con ID: ', OLD.id));
+END //
+DELIMITER ;
+
+-- Trigger para la tabla Criticas
+DELIMITER //
+CREATE TRIGGER Criticas_Insert_Trigger
+AFTER INSERT ON Criticas
+FOR EACH ROW
+BEGIN
+    INSERT INTO Registro_Operaciones (Tabla, Operacion, Detalles) VALUES ('Criticas', 'Insert', CONCAT('Se inserto una critica con ID: ', NEW.id));
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER Criticas_Update_Trigger
+AFTER UPDATE ON Criticas
+FOR EACH ROW
+BEGIN
+    INSERT INTO Registro_Operaciones (Tabla, Operacion, Detalles) VALUES ('Criticas', 'Update', CONCAT('Se actualizo una critica con ID: ', NEW.id));
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER Criticas_Delete_Trigger
+AFTER DELETE ON Criticas
+FOR EACH ROW
+BEGIN
+    INSERT INTO Registro_Operaciones (Tabla, Operacion, Detalles) VALUES ('Criticas', 'Delete', CONCAT('Se elimino una critica con ID: ', OLD.id));
+END //
+DELIMITER ;
+
+use Metacritics;
+
+-- Para la tabla Plataforma
+CALL CrearPlataforma('nombre_plataforma');
+CALL GetAllPlataformas();
+CALL UpdateByIdPlataforma(11, 'nuevo_nombre_plataforma');
+CALL GetByIdPlataforma(11);
+CALL DeleteByIdPlataforma(11);
+CALL DeleteAllPlataformas();
+
+-- Para la tabla Desarrollador
+CALL CrearDesarrollador('nombre_desarrollador');
+CALL GetAllDesarrolladores();
+CALL UpdateByIdDesarrollador(11, 'nuevo_nombre_desarrollador');
+CALL GetByIdDesarrollador(11);
+CALL DeleteByIdDesarrollador(11);
+CALL DeleteAllDesarrolladores();
+
+-- Para la tabla Publicado_por
+CALL CrearPublicadoPor('nombre_publicado_por');
+CALL GetAllPublicadoPor();
+CALL UpdateByIdPublicadoPor(11, 'nuevo_nombre_publicado_por');
+CALL GetByIdPublicadoPor(11);
+CALL DeleteByIdPublicadoPor(11);
+CALL DeleteAllPublicadoPor();
+
+-- Para la tabla Genero
+CALL CrearGenero('nombre_genero');
+CALL GetAllGeneros();
+CALL UpdateByIdGenero(11, 'nuevo_nombre_genero');
+CALL GetByIdGenero(11);
+CALL DeleteGeneroById(11);
+CALL DeleteAllGeneros();
+
+-- Para la tabla Clasificacion_Edad
+CALL CrearClasificacionEdad('nombre_edad');
+CALL GetAllClasificacionEdad();
+CALL UpdateByIdClasificacionEdad(11, 'nuevo_nombre_edad');
+CALL GetClasificacionEdadById(11);
+CALL DeleteByIdClasificacionEdad(11);
+CALL DeleteAllClasificacionEdad();
+
+-- Para la tabla Tipo_Plataforma
+CALL CrearTipoPlataforma('nombre_tipo_plataforma');
+CALL GetAllTipoPlataforma();
+CALL UpdateByIdTipoPlataforma(11, 'nuevo_nombre_tipo_plataforma');
+CALL GetTipoPlataformaById(11);
+CALL DeleteByIdTipoPlataforma(11);
+CALL DeleteAllTipoPlataforma();
+
+-- Para la tabla Videojuegos
+CALL CrearVideojuego('nombre_videojuego', '2024-05-26', 80, 'Meta', 8.5, 'Usuario', 1, 1, 1, 1, 1, 1); -- Se tiene que ejecutar anter de eliminar las FKs
+CALL GetAllVideojuegos();
+CALL UpdateByIdVideojuego(11, 'nuevo_nombre_videojuego', '2024-05-27', 85, 'Meta', 9.0, 'Usuario', 2, 2, 2, 2, 2, 2);
+CALL GetVideojuegoById(11);
+CALL DeleteByIdVideojuego(11);
+CALL DeleteAllVideojuegos(); -- Se debe de eliminar Criticas primero
+
+
+-- Para la tabla Tipo_Venta
+CALL CrearTipoVenta('nombre_tipo_venta'); 
+CALL GetAllTiposVenta();
+CALL UpdateByIdTipoVenta(3, 'nuevo_nombre_tipo_venta');
+CALL GetByIdTipoVenta(3);
+CALL DeleteByIdTipoVenta(3);
+CALL DeleteAllTiposVenta();
+
+-- Para la tabla Ventas
+CALL CrearVenta(1, 1, '2024-05-26', 10, 50.00); -- Se tiene que ejecutar anter de eliminar las FKs
+CALL GetAllVentas();
+CALL UpdateByIdVenta(21, 2, 2, '2024-05-27', 20, 100.00);
+CALL GetByIdVenta(21);
+CALL DeleteByIdVenta(21);
+CALL DeleteAllVentas();
+
+-- Para la tabla Criticas
+CALL CrearCritica(1, 'Buena');
+CALL GetAllCriticas();
+CALL UpdateByIdCritica(6, 2, 'Mala');
+CALL GetByIdCritica(6);
+CALL DeleteByIdCritica(6);
+CALL DeleteAllCriticas();
+
+Select * from Registro_Operaciones;
